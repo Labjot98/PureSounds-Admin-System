9796a3bb758c289cc796f2d2db4a0b02464a0a3d
P2807808 <P2807808@my365.dmu.ac.uk>
2025-05-01 15:56:21 +0100

    added testing for Stock class.

---
+++ Testing3/tstStock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+
+            //create an instance of the class we are testing
+            clsStock aStock = new clsStock();
+
+            //test to see if it exists
+            Assert.IsNotNull(aStock);
+        }
+    }
+}
