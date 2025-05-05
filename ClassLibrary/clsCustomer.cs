a031d224fa61fd9f37ca1611f374c93ae0317750
Labjot Singh <labjotsingh2004@gmail.com>
2025-05-05 23:27:09 +0100

    Created unit tests for customer class and from there generated properties of clsCustomer with the right data type.

---
+++ ClassLibrary/clsCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsCustomer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerFullname { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+        public bool BonusEligibility { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
