a83dc04b56c695812380e1916072cbe4181bf105
Gabriel Egan <P2681912@my365.dmu.ac.uk>
2025-05-06 14:28:15 +0100

    Added some tests that showed that properties could be made.

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public int StaffID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateJoined { get; set; }
+        public DateTime DateLeft { get; set; }
+        public string Rank { get; set; }
+        public string NINumber { get; set; }
+        public bool IsFemale { get; set; }
     }
 }
--- Testing1/tstStaff.cs
+++ Testing1/tstStaff.cs
@@ -15,5 +15,97 @@ namespace Testing1
             // test to see that it exists
             Assert.IsNotNull(AStaff);
         }
+
+        [TestMethod]
+        public void StaffIDPropertyOK()
+        {
+            // create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            // create some test data to assign to the property
+            Int32 TestData = 1;
+            // assign the data to the property
+            AStaff.StaffID = TestData;
+            // test to see whether the two values are the same
+            Assert.AreEqual(AStaff.StaffID, TestData);
+        }
+
+        [TestMethod]
+        public void StaffNamePropertyOK()
+        {
+            // create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            // create some test data to assign to the property
+            string TestData = "Orson Welles";
+            // assign the data to the property
+            AStaff.Name = TestData;
+            // test to see whether the two values are the same
+            Assert.AreEqual(AStaff.Name, TestData);
+        }
+
+        [TestMethod]
+        public void StaffDateJoinedPropertyOK()
+        {
+            // create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            // create some test data to assign to the property
+            DateTime TestData = DateTime.Now;
+            // assign the data to the property
+            AStaff.DateJoined = TestData;
+            // test to see whether the two values are the same
+            Assert.AreEqual(AStaff.DateJoined, TestData);
+        }
+
+        [TestMethod]
+        public void StaffDateLeftPropertyOK()
+        {
+            // create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            // create some test data to assign to the property
+            DateTime TestData = DateTime.Now;
+            // assign the data to the property
+            AStaff.DateLeft = TestData;
+            // test to see whether the two values are the same
+            Assert.AreEqual(AStaff.DateLeft, TestData);
+        }
+
+        [TestMethod]
+        public void StaffRankPropertyOK()
+        {
+            // create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            // create some test data to assign to the property
+            string TestData = "Senior Salesperson";
+            // assign the data to the property
+            AStaff.Rank = TestData;
+            // test to see whether the two values are the same
+            Assert.AreEqual(AStaff.Rank, TestData);
+        }
+
+        [TestMethod]
+        public void StaffNINumberPropertyOK()
+        {
+            // create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            // create some test data to assign to the property
+            string TestData = "NH 323334 B";
+            // assign the data to the property
+            AStaff.NINumber = TestData;
+            // test to see whether the two values are the same
+            Assert.AreEqual(AStaff.NINumber, TestData);
+        }
+
+        [TestMethod]
+        public void StaffIsFemaleOK()
+        {
+            // create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            // create some test data to assign to the property
+            Boolean TestData = false;
+            // assign the data to the property
+            AStaff.IsFemale = TestData;
+            // test to see whether the two values are the same
+            Assert.AreEqual(AStaff.IsFemale, TestData);
+        }
+
     }
 }
