d548d72583a5f488b5961890eb7787957f87ac39
Labjot Singh <labjotsingh2004@gmail.com>
2025-05-08 19:36:15 +0100

    Created a simple presentation layer and added controls to it

--- AdminSystem/CustomerViewer.aspx.cs
+++ AdminSystem/CustomerViewer.aspx.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //create an instance of clsCustomer
+        clsCustomer ACustomer = new clsCustomer();
+
+        //get the data from session object
+        ACustomer = (clsCustomer)Session["ACustomer"];
+
+        //display the customer full name for this entry
+        Response.Write(ACustomer.CustomerId);
+        Response.Write(ACustomer.CustomerFullname);
+        Response.Write(ACustomer.Email);
+        Response.Write(ACustomer.Address);
+        Response.Write(ACustomer.Password);
+        Response.Write(ACustomer.CreatedOn);
+        Response.Write(ACustomer.BonusEligibility);
+
+
 
     }
 }
