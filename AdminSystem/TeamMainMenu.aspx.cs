fa97c33d7e9403db0437d427a2aefa586f5145ff
Gabriel Egan <P2681912@my365.dmu.ac.uk>
2025-05-26 19:00:34 +0100

    Main Menu completed

--- AdminSystem/TeamMainMenu.aspx.cs
+++ AdminSystem/TeamMainMenu.aspx.cs
@@ -11,4 +11,33 @@ public partial class TeamMainMenu : System.Web.UI.Page
     {
 
     }
+
+    protected void btnCustomer_Click(object sender, EventArgs e)
+    {
+        // redirect the user to the Customer login page
+        Response.Redirect("CustomerLogin.aspx");
+    }
+    protected void btnOrders_Click(object sender, EventArgs e)
+    {
+        // redirect the user to the Orders login page
+        Response.Redirect("OrderLogin.aspx");
+    }
+
+    protected void btnStaff_Click(object sender, EventArgs e)
+    {
+        // redirect the user to the Staff login page
+        Response.Redirect("StaffLogin.aspx");
+    }
+
+    protected void btnStock_Click(object sender, EventArgs e)
+    {
+        // redirect the user to the Stock login page
+        Response.Redirect("StockLogin.aspx");
+    }
+
+    protected void btnSupplier_Click(object sender, EventArgs e)
+    {
+        // redirect the user to the Supplier login page
+        Response.Redirect("SupplierLogin.aspx");
+    }
 }
